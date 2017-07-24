24cabd8048449f1a03974077536589f21b2d69d4
Derek Shao <shao.derek.now@gmail.com>
2017-07-24 15:40:13 -0400

    formatting and comment

--- xkcd/xkcd.py
+++ xkcd/xkcd.py
@@ -1,4 +1,4 @@
-#downloads xkcd comic
+#downloads today's xkcd comic
 
 import requests, bs4, datetime
 
@@ -9,7 +9,9 @@ def getPage():
 
 def createSoup(request):
 	xkcdSoup = bs4.BeautifulSoup(request.text)
+	#get all elements with 'img'
 	imgElems = xkcdSoup.select('img')
+	#get attribute of img elem
 	url = imgElems[1].get('src')
 	return url
 
@@ -17,13 +19,19 @@ def mkImgRequest(url):
 	imgRes = requests.get('http:' + url)
 	return imgRes
 
-	
-
+#datetime object - gets today's date
 today = datetime.date.today()
+
+#stores request for page xkcd.com
 page = getPage()
+
+#stores the url of the xkcd comic for today
 imgUrl = createSoup(page)
+
+#store request for today's comic
 image = mkImgRequest(imgUrl)
 
+#format date string
 todayExtension = today.strftime('%Y_%m_%d')
 
 xkcd_comic = open('xkcd_' + todayExtension + '.png', 'wb')
@@ -32,4 +40,6 @@ for chunk in image.iter_content(100000):
 	xkcd_comic.write(chunk)
 
 xkcd_comic.close()
+
+
 
