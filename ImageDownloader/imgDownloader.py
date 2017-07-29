f0176244f0a25e150647cd7c76a074fd773fdcc2
Derek Shao <shao.derek.now@gmail.com>
2017-07-28 20:00:43 -0400

    Initial Commit - ImageDownloaer

---
+++ ImageDownloader/imgDownloader.py
@@ -0,0 +1,38 @@
+#google image downloader
+
+import requests
+from selenium import webdriver
+
+def main():
+
+	print("Search Google images...")
+	query = str(input())
+
+	browser = webdriver.Firefox()
+	browser.get("www.google.com/images")
+
+	#TODO: find html idfor the search bar
+
+	search = browser.find_element_by_id("search-bar")
+	search.send_keys(query)
+	search.submit()
+
+	#TODO: iterate through all images found from serach
+	#TODO: download all 
+
+def getImgRequest(url):
+	res = requests.get(url)
+
+def downloadImg(requests):
+
+	img = open("googleimage.png", "wb")
+
+	for chunk in requests.iter_content(100000):
+		img.write(chunk)
+
+	img.close()
+
+
+if __name__ == "__main__":
+	#opening
+	main()
