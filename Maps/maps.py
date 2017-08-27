65f0d4dc9d7ca7e1ad4d63d111dc4108f8f9ea66
Derek Shao <shao.derek.now@gmail.com>
2017-08-27 09:47:18 -0400

    Google map search

---
+++ Maps/maps.py
@@ -0,0 +1,11 @@
+import sys, pyperclip, webbrowser
+
+if (len(sys.argv) > 1):
+	#get address from command line
+	address = ''.join(sys.argv[1:])
+else:
+	#get address from copy
+	address = pyperclip.paste()
+
+
+webbrowser.open('http://www.google.com/maps/place/' + address)
