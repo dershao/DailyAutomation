69c04fa2cbfaac379758f34151092cbe42d8b7fe
Derek Shao <shao.derek.now@gmail.com>
2017-08-29 15:40:28 -0400

    DRY

--- Weather/weather.py
+++ Weather/weather.py
@@ -4,18 +4,6 @@ import requests, json, sys
 API_key = "a78cb5de3d831abd304abb2ff1d0dcc0"
 
 
-def requestWeatherData(city, country):
-
-	res = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country  + "&appid=" + API_key)
-	res.raise_for_status()
-	return res
-
-def loadWeatherJson(request):
-
-	weatherJson = json.loads(request.text)
-
-	return weatherJson
-
 def displayWeatherData(mJson):
 
 	name_city = mJson["name"].upper()
@@ -42,6 +30,22 @@ def displayWeatherData(mJson):
 	print("High: " + str(round(high_temp, 2)) + "\n")
 	print("Low: " + str(round(low_temp, 2)) + "\n")
 
+def loadWeatherJson(request):
+
+	weatherJson = json.loads(request.text)
+
+	displayWeatherData(weatherJson)
+
+def requestWeatherData(city, country):
+
+	res = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country  + "&appid=" + API_key)
+	res.raise_for_status()
+	loadWeatherJson(res)
+
+def locationPrompt():
+	city = str(input("Enter a city: "))
+	country = str(input("Enter a country: "))
+	return (city, country)
 
 
 def main():
@@ -49,16 +53,16 @@ def main():
 	if (len(sys.argv) >= 2):
 		city = sys.argv[1]
 		country = sys.argv[2]
+		requestWeatherData(city, country)
 	else:
-		city = str(input("Enter a city: "))
-		country = str(input("Enter a country: "))
-
-	weatherRes = requestWeatherData(city, country)
-	weatherJson = loadWeatherJson(weatherRes)
-
-	displayWeatherData(weatherJson)
-
-
+		while True:
+			location = locationPrompt()
+			requestWeatherData(location[0], location[1])
+			
+			again = str(input("Search weather data from new location? [Y/n]"))
+			
+			if again.upper() != "Y":
+				break;
 
 if __name__ ==  "__main__":
 	main()
